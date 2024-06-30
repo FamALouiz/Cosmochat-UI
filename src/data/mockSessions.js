ab63982e54aed56ab192b57354aeb49e85d7ad8f
Fam Awad <fam@awadlouis.com>
2024-06-30 20:56:21 +0200

    Updated mock data

---
+++ src/data/mockSessions.js
@@ -0,0 +1,91 @@
+import ROLES from "../constants/Roles";
+import CONV_DIRECTION from "../constants/ConversationDirection";
+
+const mockSessions = [
+  {
+    startedAt: "2024-08-03",
+    messages: [
+      {
+        message: "Hello! I am your personal assistant... How can I help?",
+        sender: ROLES.ChatGPT,
+        direction: CONV_DIRECTION.Incoming,
+      },
+      {
+        message: "How are you doing today?",
+        sender: ROLES.User,
+        direction: CONV_DIRECTION.Outgoing,
+      },
+      {
+        message:
+          "I'm doing well, thank you! How about you? Working on any interesting projects lately?",
+        sender: ROLES.ChatGPT,
+        direction: CONV_DIRECTION.Incoming,
+      },
+    ],
+  },
+  {
+    startedAt: "2024-08-04",
+    messages: [
+      {
+        message: "Hello! I am your personal assistant... How can I help?",
+        sender: ROLES.ChatGPT,
+        direction: CONV_DIRECTION.Incoming,
+      },
+      {
+        message: "How are you doing today?",
+        sender: ROLES.User,
+        direction: CONV_DIRECTION.Outgoing,
+      },
+      {
+        message:
+          "I'm doing well, thank you! How about you? Working on any interesting projects lately?",
+        sender: ROLES.ChatGPT,
+        direction: CONV_DIRECTION.Incoming,
+      },
+    ],
+  },
+  {
+    startedAt: "2024-08-05",
+    messages: [
+      {
+        message: "Hello! I am your personal assistant... How can I help?",
+        sender: ROLES.ChatGPT,
+        direction: CONV_DIRECTION.Incoming,
+      },
+      {
+        message: "How are you doing today?",
+        sender: ROLES.User,
+        direction: CONV_DIRECTION.Outgoing,
+      },
+      {
+        message:
+          "I'm doing well, thank you! How about you? Working on any interesting projects lately?",
+        sender: ROLES.ChatGPT,
+        direction: CONV_DIRECTION.Incoming,
+      },
+    ],
+  },
+  {
+    startedAt: "2024-08-06",
+    messages: [
+      {
+        message: "Hello! I am your personal assistant... How can I help?",
+        sender: ROLES.ChatGPT,
+        direction: CONV_DIRECTION.Incoming,
+      },
+      {
+        message: "How are you doing today?",
+        sender: ROLES.User,
+        direction: CONV_DIRECTION.Outgoing,
+      },
+      {
+        message:
+          "I'm doing well, thank you! How about you? Working on any interesting projects lately?",
+        sender: ROLES.ChatGPT,
+        direction: CONV_DIRECTION.Incoming,
+      },
+    ],
+  },
+];
+
+export default mockSessions;
