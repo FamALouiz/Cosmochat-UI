c1fcae7579818f7fe4f549ea7e58b9b18d787301
Fam Awad <fam@awadlouis.com>
2024-07-03 16:43:58 +0200

    Added progress circle

---
+++ src/components/ProgressCircle/ProgressCircle.jsx
@@ -0,0 +1,21 @@
+import { Box, useTheme } from "@mui/material";
+
+function ProgressCircle({ progress = 0.75, size = 40 }) {
+  const theme = useTheme();
+  const angle = progress * 360;
+
+  return (
+    <Box
+      sx={{
+        background: `radial-gradient(red 55%, transparent 56%)
+        conic-gradient(transparent 0deg ${angle}deg, blue ${angle}deg 360),
+        green`,
+        borderRadius: "50%",
+        width: `${size}px`,
+        height: `${size}px`,
+      }}
+    />
+  );
+}
+
+export default ProgressCircle;
